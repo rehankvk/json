5e50097c5614eb0f78878810b3fe9d4abc228826
rehankvk <102939245+rehankvk@users.noreply.github.com>
2023-05-10 20:50:25 +0700

    update redirect

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Comments from "./pages/Comments.vue";
 import Albums from "./pages/Albums.vue";
 import Todos from "./pages/Todos.vue";
 import Users from "./pages/Users.vue";
+
 const routes = [
   { path: "/", component: HomePage },
   { path: "/posts", component: Posts },
@@ -12,9 +13,13 @@ const routes = [
   { path: "/albums", component: Albums },
   { path: "/todos", component: Todos },
   { path: "/users", component: Users },
+  { path: '*', redirect: '/'},
+  { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
 export default router;
