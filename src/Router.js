02e6de295932b0c88f87d3af3fbc3c3112f703c8
rehankvk <102939245+rehankvk@users.noreply.github.com>
2023-05-12 09:29:53 +0700

    change title

--- src/Router.js
+++ src/Router.js
@@ -2,37 +2,37 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 
 const routes = [
-  { path: "/", component: HomePage, name: "home", meta: { title: "Home Page" } },
+  { path: "/", component: HomePage, name: "home", meta: { title: "Home Page" }, },
 
   {
     path: "/posts",
     component: () => import("./pages/Post.vue"),
     name: "posts",
-    meta: { title: "Posts Page" }
+    meta: { title: "Posts Page" },
   },
   {
     path: "/comments",
     component: () => import("./pages/Comments.vue"),
     name: "comments",
-    meta: { title: "Comments Page" }
+    meta: { title: "Comments Page" },
   },
   {
     path: "/albums",
     component: () => import("./pages/Albums.vue"),
     name: "albums",
-    meta: { title: "Albums Page" }
+    meta: { title: "Albums Page" },
   },
   {
     path: "/todos",
     component: () => import("./pages/Todos.vue"),
     name: "todos",
-    meta: { title: "Todos Page" }
+    meta: { title: "Todos Page" },
   },
   {
     path: "/users",
     component: () => import("./pages/Users.vue"),
     name: "users",
-    meta: { title: "Users Page }
+    meta: { title: "Users Page },
   },
   {
     path: '/:catchAll(.*)',
